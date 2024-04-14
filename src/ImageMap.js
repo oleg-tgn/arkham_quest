3fe8b9c6a920265e938dc0477c97e80785de4b07
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2024-04-14 18:39:47 +0400

    click on image zone out of area

--- src/ImageMap.js
+++ src/ImageMap.js
@@ -6,7 +6,6 @@ const ImageMap = () => {
     const [selectedBrick, setSelectedBrick] = useState('');
     const [tooltip, setTooltip] = useState({ visible: false, text: '', x: 0, y: 0 });
     const imgRef = useRef(null);  // Reference to the image element
-    const [highlightedArea, setHighlightedArea] = useState(null);
 
 
     useEffect(() => {
@@ -46,6 +45,39 @@ const ImageMap = () => {
         setTooltip({ visible: true, x, y, text: `${key} - ${object.name}` });
     };
 
+    function isPointInPolygon(polygon, x, y) {
+        let inside = false;
+        for (let i = 0, j = polygon.length - 2; i < polygon.length; i += 2) {
+            let xi = polygon[i], yi = polygon[i + 1];
+            let xj = polygon[j], yj = polygon[j + 1];
+    
+            let intersect = ((yi > y) != (yj > y))
+                && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+            if (intersect) inside = !inside;
+            j = i;
+        }
+        return inside;
+    }
+
+    const handleImageClick = event => {
+        const rect = imgRef.current.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        const areas = document.querySelectorAll('area');
+        let isAreaClicked = false;
+
+        areas.forEach(area => {
+            const coords = area.coords.split(',').map(Number);
+            if (isPointInPolygon(coords, x, y)) {
+                isAreaClicked = true;
+            }
+        });
+
+        if (!isAreaClicked) {
+            setTooltip(prev => ({ ...prev, visible: false }));
+        }
+    };
+
     const handleImageLoaded = () => {
         updateCoords();  // This will now be called when the image is fully loaded
     };    
@@ -56,10 +88,8 @@ const ImageMap = () => {
             
                 <img ref={imgRef} src={arkhemMap}
                     useMap="#Map" alt="Pyramid"
-                    onLoad={handleImageLoaded} />
-
-                {/* <img ref={imgRef} src="https://res.cloudinary.com/positionrelativ/image/upload/v1492379605/pyramid_1_fu4idd.png"
-                     useMap="#Map" alt="Pyramid" /> */}
+                    onLoad={handleImageLoaded} 
+                    onClick={handleImageClick}/>
 
                 <map name="Map" id="Map">
                     {/* Make sure to include the `data-original-coords` attribute for each area */}
