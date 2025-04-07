8b327cd5ebf495d1f11d6955f9e069232556d6a0
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2025-04-07 23:44:23 +0400

    Try to fix netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // 👇 это ключевая строка
-    rollupOptions: {
-      // отключение использования native binary
-      external: ['@rollup/rollup-linux-x64-gnu']
-    }
-  }
 });
