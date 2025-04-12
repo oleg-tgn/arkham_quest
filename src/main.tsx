8cb827054fafc5fdfaeb9c3dd51ca042dbcd765d
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2025-04-12 20:24:10 +0400

    Fix VIte + Tailwind: Recreate app config

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.tsx'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Root element not found');
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
