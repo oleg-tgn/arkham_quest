5ee9f70039adc38dbefeecbcbce128a4b587929e
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2025-04-25 01:05:54 +0400

    [WP] Font config

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const Button = ({ children, variant, onClick }: Props) => {
   const classes: Record<string, string> = {
     primary:
-      'w-90 px-4 py-6 my-2 bg-contain bg-[url("/img/button.png")] bg-no-repeat bg-cover bg-center brightness-85 hover:brightness-100 cursor-pointer text-white text-xl',
+      'w-90 px-4 py-6 my-2 bg-contain bg-[url("/img/button.png")] bg-no-repeat bg-cover bg-center brightness-85 hover:brightness-100 cursor-pointer text-white text-xl font-roboto',
     secondary: '',
     story:
       'w-full px-4 py-2 my-2 border-b-2 border-black/50 hover:border-black cursor-pointer text-left text-xl text-black',
